27aaca67f2766a5d9e29a25cef8a64ebb1209e84
zhangGuojin <zhanggjjim@163.com>
2016-12-26 18:14:20 +0800

    增加mongoose

--- app.js
+++ app.js
@@ -1,60 +1,71 @@
+/**
+ * Created by wangxr on 16/12/15.
+ */
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var _User = Schema({
+  app_id: { type: String, index : true }, // app info
+  phone: { type: String, index: true },
+  country_code: String,
+  avatar:String,
+  fullname: String,
+  username: String,
+  lock_username: { type: Number, default: -1 },
+  password: String,
+  birthday: Date,
+  /* 1- 男 0-女 */
+  gender: { type: Number, default: -1 },
+  introduce: String,
+  industry: String,
+  // sns
+  email: { type: String, index: true },
+  github: String,
+  linkedin: String,
+  twitter: String,
+  facebook: String,
+  weixin: String,
+  weibo: String,
+  search_text:String,
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  // 0 普通人, 1 创业者, 2 投资人, -999 机器人
+  type: { type: Number, default: 0 },
+  // 创建经费群配额
+  fund_raising_channel_own_quota: Number,
 
-app.use('/', routes);
-app.use('/users', users);
+  // redundant field
+  company: String,
+  title: String,
+  city: String,
+  // cms
+  /* 1 有权限*/
+  is_admin: { type: Number, default: 0 },
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // 私聊设置 0 无条件, 1 需要发送名片
+  direct_chat_status: { type: Number, default: 0 },
+  _status: { type: Number, default: 1 }, // -1 受限用户
+  // 是否运营人员, 可连接客服机器人
+  is_operator: Number,
+  // 是否开启点赞变收藏  1. 开启; -1 关闭
+  like_to_collect : { type: Number, default: 1 }
+}, { collection: 'user' });
+
+mongoose.Promise = global.Promise;
+
+db.on('error',console.error.bind(console,'连接错误:'));
+db.once('open',function(){
+  //一次打开记录
 });
 
-// error handlers
+var model = db.model('User', _User);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+app.get('/', function (req, res) {
+  model.findById({_id: '55c8c86f9cd1320314a03047'}, function(err, doc){
+    console.log(doc);
+    res.send(doc);
   });
 });
-
 
 module.exports = app;
